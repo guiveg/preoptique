5d03ed72f9b3ce256832bc7b32e1bde9853bbaa3
Guillermo Vega Gorgojo <guiveg@ifi.uio.no>
2018-01-16 20:16:45 +0100

    Updated images

--- etc/data/parameters.js
+++ etc/data/parameters.js
@@ -4,20 +4,17 @@
 
 var parameters = new Object();
 
-// authorization service
-parameters.cerberus = "https://localhost:8000/cerberus"; // URI of the authorization server
-parameters.sessionTimeout = 20; // 20 minutes
-
-
 // SPARQL endpoint
 parameters.sparqlBase = "http://localhost:3030/ahus2/query";
 // HTTP method employed in the SPARQL queries
 parameters.httpMethod = "GET";
 
-
-// text engine
+// Solr text engine
 parameters.textEngine = "http://localhost:8983/solr/ahus2/select";
 
+// authorization service
+parameters.cerberus = "https://localhost:8000/cerberus"; // URI of the authorization server
+parameters.sessionTimeout = 20; // 20 minutes
 
 // logging (see http://docs.shopify.com/manual/configuration/store-customization/get-a-visitors-location)
 parameters.geoipservice = "http://freegeoip.net/json/";
